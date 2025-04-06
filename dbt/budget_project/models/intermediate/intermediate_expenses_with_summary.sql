46e3555ae19723e3d099e93f1ca192e55554380b
Hung-En Liao <Hung-En Liao>
2025-04-06 17:33:17 -0300

    dbt category updates

--- dbt/budget_project/models/intermediate/intermediate_expenses_with_summary.sql
+++ dbt/budget_project/models/intermediate/intermediate_expenses_with_summary.sql
@@ -3,9 +3,9 @@
 SELECT
     *,
     CASE
-        WHEN category = 'House' THEN 'House'
+        WHEN category = 'Home' THEN 'House'
         WHEN category = 'Saved for Love' THEN 'Saved for Love'
-        WHEN category IN ('Donation', 'Gift') THEN 'Donation and Gift'
+        WHEN category IN ('Donation', 'Gifts') THEN 'Donation and Gifts'
         WHEN category = 'Education' THEN 'Education'
         WHEN category = 'Traveling' THEN 'Traveling'
         ELSE 'Daily Expense'
