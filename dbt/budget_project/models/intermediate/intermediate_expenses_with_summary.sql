4efdd5957d24d751361b3b2e91d9659822aa8bb5
Hung-En Liao <Hung-En Liao>
2025-04-10 17:43:23 -0300

    dbt data type

--- dbt/budget_project/models/intermediate/intermediate_expenses_with_summary.sql
+++ dbt/budget_project/models/intermediate/intermediate_expenses_with_summary.sql
@@ -4,10 +4,10 @@ SELECT
     *,
     CASE
         WHEN category = 'Home Deposit' THEN 'House'
-        WHEN category = 'Saved For Love' THEN 'Saved for Love'
+        WHEN category = 'Saved for Love' THEN 'Saved for Love'
         WHEN category IN ('Donation', 'Gifts') THEN 'Donation and Gifts'
         WHEN category = 'Education' THEN 'Education'
         WHEN category = 'Traveling' THEN 'Traveling'
-        ELSE 'Daily Expense'
+        ELSE 'Daily Expenses'
     END AS summary_category
 FROM {{ source('public', 'expense') }}
