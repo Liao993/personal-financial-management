b798a2564c551fa66c2d38b4ebfb1703eb0de617
Hung-En Liao <Hung-En Liao>
2025-04-11 21:59:40 -0300

    pdf to table

--- app/modules/upload_pdf/component/pipeline/pipeline.py
+++ app/modules/upload_pdf/component/pipeline/pipeline.py
@@ -1,20 +1,16 @@
 import streamlit as st
 from modules.upload_pdf.component.pipeline.rbc.rbc_extracted import rbc_extracted
-
+from modules.upload_pdf.component.pipeline.rbc.rbc_transformed import rbc_transformed
 
 def pipeline(bank, pdf_file):
-  st.success("Your data is being processed")
-  for file in pdf_file:
-    st.info(f"Processing file before extracted: {file.name}")
-  if bank == "RBC":
-    extracted_data = rbc_extracted(pdf_file)
-    if extracted_data:
-        for filename, text in extracted_data.items():
-            st.subheader(f"Content of {filename}:")
-            st.text_area("PDF Text", text, height=300)
-    #transfromed_data = rbc_transformed(extracted_data)
-    #displayed_data = rbc_display(transfromed_data)
-    #loaded_data = rbc_load(displayed_data)
 
+  st.success("Your data is being processed")
+
+  if bank == "RBC":
+      extracted_data = rbc_extracted(pdf_file)
+      transformed_data = rbc_transformed(extracted_data)
+      st.table(transformed_data)
   elif bank == "TD":
-    st.write("TD pipeline")
+      st.write("TD pipeline")
+
+ 
