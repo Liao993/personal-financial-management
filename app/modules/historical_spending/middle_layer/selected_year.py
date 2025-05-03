d7bb5cf6c07ec47f129fb4d93df2cb51180d1068
Hung-En Liao <Hung-En Liao>
2025-05-03 20:04:35 -0300

    get files back

---
+++ app/modules/historical_spending/middle_layer/selected_year.py
@@ -0,0 +1,7 @@
+import streamlit as st # type: ignore
+
+def selected_year_choice(key):
+  year_list = [2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034, 2035]
+  selected_year = st.selectbox("Please select a year", year_list, key=key)
+
+  
