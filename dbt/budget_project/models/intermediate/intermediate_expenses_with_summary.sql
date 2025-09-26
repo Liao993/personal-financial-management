ad13f3ceaa0e4df9a3cc05aac52c4c3dc1cfe4fd
Henry <henry@mac.home>
2025-09-26 19:06:03 -0300

    dbt change for save for love

--- dbt/budget_project/models/intermediate/intermediate_expenses_with_summary.sql
+++ dbt/budget_project/models/intermediate/intermediate_expenses_with_summary.sql
@@ -8,6 +8,7 @@ SELECT
         WHEN category IN ('Donation', 'Gifts') THEN 'Donation and Gifts'
         WHEN category = 'Education' THEN 'Education'
         WHEN category = 'Traveling' THEN 'Traveling'
+        WHEN category = 'Saved For Love' THEN 'Saved For Love'
         ELSE 'Daily Expenses'
     END AS summary_category
 FROM {{ source('public', 'expense') }}
