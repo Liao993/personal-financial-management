3a2fd9c90cf0cc3b0670a3002eb96a7d3b7fd170
Henry <henry@mac.home>
2025-12-30 16:57:53 -0400

    add macro to dbt

---
+++ dbt/budget_project/macros/get_summary_category.sql
@@ -0,0 +1,12 @@
+{% macro get_summary_category(col) %}
+    {#1. List all Category #}
+    {% set simple_category = ['Saved For Love', 'Education', 'Traveling', "House"] %}
+    CASE 
+        {# For Loop for 1:1 Map Category #}
+        {% for cat in simple_category %}
+            WHEN {{col}} = '{{cat}}' THEN '{{cat}}'
+        {% endfor %}
+        WHEN {{col}} IN ('Donation', 'Gifts') THEN 'Donation and Gifts'
+        ELSE 'Daily Expenses'
+    END
+{% endmacro %}
--- dbt/budget_project/models/intermediate/intermediate_expenses_with_summary.sql
+++ dbt/budget_project/models/intermediate/intermediate_expenses_with_summary.sql
@@ -1,20 +1,15 @@
 {{ config(materialized='view') }}
 
+
 SELECT
     *,
+    {# Calculate amount I spend #}
     (amount / NULLIF(COALESCE(amount_for_number_of_travelers, 1), 0)) * COALESCE(paid_for_number_of_travlerers, 1) AS amount_I_spend,
-    CASE
-        --WHEN category = 'House' THEN 'House'
-        -- for data clean-up purposes
-        WHEN category = 'Saved for Love' THEN 'Saved for Love'
-        WHEN category = 'Saved For Love' THEN 'Saved For Love'
-        WHEN category IN ('Donation', 'Gifts') THEN 'Donation and Gifts'
-        WHEN category = 'Education' THEN 'Education'
-        WHEN category = 'Traveling' THEN 'Traveling'
-        
-        ELSE 'Daily Expenses'
-    END AS summary_category,
 
+    {# Call the Macro for Summary Category #}
+    {{ get_summary_category('category') }} AS summary_category,
+
+    {#  House Summary Category #}
     CASE
         WHEN house_category = 'Mortgage' THEN 'Mortgage'
         WHEN house_category = 'Extra Mortgage' THEN 'Extra Mortgage'
@@ -23,4 +18,6 @@ SELECT
         WHEN house_category LIKE '%Tax%' THEN 'Tax'
         ELSE 'Other'
     END AS house_summary_category
+
 FROM {{ source('public', 'expense') }}
+
