83fddcf85678f368d82bbcf91c0692c8f2de2f20
Henry <henry@mac.home>
2026-05-09 23:42:44 -0300

    new btn for withdrawl in statement viwer

--- app/modules/statement_viewer/component/quick_viewer_btn.py
+++ app/modules/statement_viewer/component/quick_viewer_btn.py
@@ -13,19 +13,32 @@ def quick_viewer_section():
    
     expense_btn()
     transaction_btn()
-    query_buttons = st.columns(2)        
+    query_buttons = st.columns(4)        
     with query_buttons[0]:
-        if st.button("💰 View All Income", use_container_width=True):
-            execute_predefined_query("SELECT *, SUM(amount) OVER (PARTITION BY EXTRACT(YEAR FROM date)) AS year_total FROM income ORDER BY date DESC")
-    with query_buttons[1]:
         if st.button("✍️ Write Customized Query", use_container_width=True, key="custom_query_btn",type="primary"):
             reset_to_query_mode() 
+    with query_buttons[1]: 
+        if st.button("💰 View All Income", use_container_width=True):
+            execute_predefined_query("SELECT *, SUM(amount) OVER (PARTITION BY EXTRACT(YEAR FROM date)) AS year_total FROM income ORDER BY date DESC")
+
+    with query_buttons[2]:
+        if st.button("📈 All Deposit", use_container_width=True):
+            execute_predefined_query("""
+                                        SELECT transaction_id, date, fund_category, amount, transaction_type, account_name, source_notes, transfer_to_account,
+                                            sum(amount) over (partition by fund_category) AS fund_category_total_amount
+                                        FROM transactions 
+                                        WHERE transaction_type = 'Deposit'
+                                        ORDER BY fund_category, date DESC
+                                     """)
+    
+    with query_buttons[3]:
+        if st.button("📈 All Transfer", use_container_width=True):
+            execute_predefined_query("""
+                                        SELECT transaction_id, date, fund_category, amount, transaction_type, account_name, source_notes, transfer_to_account,
+                                            sum(amount) over (partition by fund_category) AS fund_category_total_amount
+                                        FROM transactions 
+                                        WHERE transaction_type Like '%Transfer%'
+                                        ORDER BY date DESC, fund_category, transaction_id DESC
+                                     """)  
     st.markdown("---")
 
-
-"""
-SELECT column_name, data_type, is_nullable 
-FROM information_schema.columns 
-WHERE table_name = 'income'
-ORDER BY ordinal_position;
-"""
